ffc9a1e53257eb54c38d0050a07a7cde80c9317d
adamperry-hub <ambiguous144@gmail.com>
2021-03-08 21:30:16 -0800

    up to middle

--- js/index.js
+++ js/index.js
@@ -44,12 +44,99 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 // const nav = document.querySelector("nav");
 // console.log(nav);
 
-const navy = document.querySelectorAll('nav a');
-console.log(navy);
+const nav = document.querySelectorAll('nav a');
+// console.log(nav);
 
-let navy2 = navy[0]
-console.log(navy2);
+let firstItem = nav[0]; // Services
 
-navy2.textContent = siteContent['nav']['nav-item-1']
+firstItem.textContent = siteContent['nav']['nav-item-1']
 
-navy
+let secondItem = nav[1];
+secondItem.textContent = siteContent['nav']['nav-item-2']
+
+let thirdItem = nav[2];
+thirdItem.textContent = siteContent['nav']['nav-item-3']
+
+let fourthItem = nav[3];
+fourthItem.textContent = siteContent['nav']['nav-item-4']
+
+let fifthItem = nav[4];
+fifthItem.textContent = siteContent['nav']['nav-item-5']
+
+let sixthItem = nav[5];
+sixthItem.textContent = siteContent['nav']['nav-item-6']
+//end of navigation
+let headText = document.querySelector('h1');
+// console.log(headText);
+
+// siteContent["cta"]["h1"]
+headText.textContent = siteContent['cta']['h1']
+
+// capture the button - querySelector '.whatevertheclassis button'
+const butt = document.querySelector('button');
+// console.log(butt);
+
+
+butt.textContent = siteContent['cta']['button']
+
+const image = document.querySelector('.cta img');
+// console.log(image);
+
+image.setAttribute('src', siteContent['cta']['img-src']);
+
+const topC = document.querySelector('.top-content');
+
+const topFirstCard = topC.childNodes[1] // div text content
+// console.log(topFirstCard)
+
+const topContent1 = topFirstCard.children[0] // h4
+// console.log(topContent1);
+// .textContent logic in HERE
+topContent1.textContent = siteContent["main-content"]["features-h4"];
+// console.log(topFirstCard);
+
+const topContent2 = topFirstCard.children[1]
+topContent2.textContent = siteContent["main-content"]["features-content"];
+// p
+// ,textContent logic in HERE
+
+
+// console.log(topContent1);
+// console.log(topContent2);
+
+topSecondCard = topC.childNodes[3]
+// console.log(topFirstCard);
+// console.log(topSecondCard);
+topContent1.textContent = siteContent["main-content"]["features-h4"];
+console.log(topSecondCard);
+
+
+const middleImage = document.getElementById("middle-img");
+middleImage.setAttribute('src', siteContent['main-content']["middle-img-src"]);
+// console.log(middleImage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const bottomthing = topSecondCard.children[1] //h4 of the bottom tect-content
+
+// .textContent --> siteContent object
+
+
